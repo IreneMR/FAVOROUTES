5a678789f334f4687ae06bdf00efa04e0bb2d722
neneando <inenee@gmail.com>
2015-06-18 18:11:30 +0200

    Pestaña Info añadida

--- Cadena_Favores/config/routes.rb
+++ Cadena_Favores/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => "chains#landing", as: "root"
   get '/chains/map' => "chains#map", as: 'map'
+  get '/chains/info' => "chains#info", as: 'info'
 
   resources :chains do
     resources :userposts
